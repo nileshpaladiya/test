524e1bd0568d0f9e4e168d0f30143ac5071e45b3
Nilesh Paladiya <nilesh@narolainfotech.com>
2017-12-14 15:43:03 +0530

    changed setting

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '3^))u3*f_$xy7$arlt!b1x3pk4w-9@#6&60&$(w8^!mwijh&ir'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','nileshpaladiya.pythonanywhere.com']
 
 
 # Application definition
